29540cbb5e6129ca992902ed506e2aa9bfe43b93
JesseHouk900 <jessejameshouk@gmail.com>
2018-09-03 15:25:45 -0600

    Completed Program 1 for Dr. Stringfellow: RadioGUI exercise 2.11

---
+++ RadioGUIAssignment1/RadioGUIAssignment1/RadioForm.cs
@@ -0,0 +1,25 @@
+﻿/*/////////////////////////////////////////////////////////////////////////////
+Jesse Houk:         Program 1: 2.11 Radio GUI
+Purpose: Make a GUI for figure 2.40 in VIsual C# How To Program Sixth Edition.
+
+/////////////////////////////////////////////////////////////////////////////*/
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RadioGUIAssignment1
+{
+    public partial class RadioForm : Form
+    {
+        public RadioForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
