822cd826bb99761a2bc9436a18832fbd31d7a16e
JesseHouk900 <jessejameshouk@gmail.com>
2018-09-10 12:30:06 -0500

    Added comments

--- sum_average_smallest_largest_Houk.cs
+++ sum_average_smallest_largest_Houk.cs
@@ -1,7 +1,8 @@
 /*/////////////////////////////////////////////////////////////////////////////
 Jesse Houk:    Assignment 2:    Dr. Stringfellow:    4143
 
-Get 3 integers from the user and display the sum, average, smalles, and largest numbers.
+Problem: Get 3 integers from the user and display the sum, average, smalles, and largest
+	numbers.
 
 /////////////////////////////////////////////////////////////////////////////*/
 
@@ -10,6 +11,7 @@ using System;
 
 class NumberCrunch {
     static void Main(string [] args) {
+		// sum, average, smallest, largest are all nullable integers
         int? sum, average, smallest, largest;
         int [] nums = new int [3];
         Console.WriteLine("Please enter 3 numbers");
@@ -26,7 +28,8 @@ class NumberCrunch {
         largest = Largest(nums, 3);
         Print("Largest", largest);
     }
-
+	// Take in a integer array with its size and return a nullable integer
+	// Function will sum the elements in an array and return the sum
     static int? Sum(int [] v, int size) {
         if (size > 0) {
             int s = 0;
@@ -37,12 +40,17 @@ class NumberCrunch {
         }
         else return null;
     }
+	// Take in a nullable s, representing the sum, and a size variable that
+		// will divide the sum. Return type can be null, but otherwise
+		// represents the average of the elements from some array.
     static int? Average(int? s, int size) {
         if (size > 0 && s != null) {
             return s / size;
         }
         else return null;
     }
+	// Take in an array of integers with a size and return a nullable value
+		// that should be the smallest integer in the array
     static int? Smallest(int [] v, int size) {
         int s;
         if (size > 0) {
@@ -56,6 +64,8 @@ class NumberCrunch {
         }
         else return null;
     }
+	// Take in an array and its size and return a nullable integer value.
+		// The return element represents the largest value in the array
     static int? Largest(int [] v, int size) {
         int l;
         if (size > 0) {
@@ -69,6 +79,9 @@ class NumberCrunch {
         }
         else return null;
     }
+	// A print function that helps pring variable data. Pass in the name
+		// of the variable and an associated, nullable value. If the integer
+		// is null, print ERROR
     static void Print(string message, int? n) {
         Console.Write(message);
         Console.Write(": ");
